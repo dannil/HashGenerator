15ff770da65b014bda34d4ecd5baa37c86e8b879
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-08 09:26:42 +0200

    Added basic interface for index page

--- index.php
+++ index.php
@@ -1,20 +1,50 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8"/>
+        <meta charset="UTF-8">
         <title>HashGenerator</title>
-        <link rel="stylesheet" href="css/style.css"/>
+        <link rel="stylesheet" href="css/style.css">
+        <script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('input[type="checkbox"]').click(function(){
+                    if($(this).attr("value")=="salt"){
+                        $(".salt").toggle();
+                    }
+                });
+            });
+        </script>
     </head>
     <body>
-        <?php
+        <div id="container">
+            <div id="center_container">
+                <form method="POST">
+                    <p>String to hash <input type="text" name="stringToHashField" size="46"></p>
+                    <p>Use salt? <input type="checkbox" name="saltField" value="salt"></p>
+                    <div class="salt">
+                        <p>Salt <input type="text" name="salt" size="55"></p>
+                        <p>Place salt at <input type="radio" name="placeSaltAt" value="beginning">beginning 
+                                         <input type="radio" name="placeSaltAt" value="end"> end
+                    </div>
+                    <div class="button">
+                        <p><input type="submit" value="Hash string"></p>
+                    </div>
+                </form>
+                
+                <?php
+
+                function __autoload($classname) {
+                    require_once('classes/' . $classname . '.class.php');
+                }
+
+                $ripemd = new HashRIPEMD();
+                //echo "<p>" . $ripemd->getRIPEMD320Hash("See ya!") . "</p>";
+
+                ?>
+            </div>
+
+        </div>
+       
         
-        function __autoload($classname) {
-            require_once('classes/' . $classname . '.class.php');
-        }
-        
-        $ripemd = new HashRIPEMD();
-        echo $ripemd->getRIPEMD320Hash("See ya!");
-        
-        ?>
     </body>
 </html>
