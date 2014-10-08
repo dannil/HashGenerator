098564bce7c68d433403cb05f030763aa975d4cf
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-08 09:32:27 +0200

    Improved interface

--- index.php
+++ index.php
@@ -17,14 +17,18 @@
     </head>
     <body>
         <div id="container">
-            <div id="center_container">
+            <div id="header">
+                <h1>Hash Generator</h1>
+            </div>
+            <div class="border"></div>
+            <div id="contentcontainer">
                 <form method="POST">
                     <p>String to hash <input type="text" name="stringToHashField" size="46"></p>
                     <p>Use salt? <input type="checkbox" name="saltField" value="salt"></p>
                     <div class="salt">
                         <p>Salt <input type="text" name="salt" size="55"></p>
-                        <p>Place salt at <input type="radio" name="placeSaltAt" value="beginning">beginning 
-                                         <input type="radio" name="placeSaltAt" value="end"> end
+                        <p>Place salt at <input type="radio" name="placeSaltAt" value="beginning" checked>beginning 
+                                         <input type="radio" name="placeSaltAt" value="end">end
                     </div>
                     <div class="button">
                         <p><input type="submit" value="Hash string"></p>
