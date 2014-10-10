b7cb3b93ef7418dd1e2ba65af4fe58b1e29d3f51
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-10 14:43:13 +0200

    Moved logic to Hash class

--- php/classes/Hash.class.php
+++ php/classes/Hash.class.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Hash;
+
 /**
  * Description of Hash
  *
@@ -23,16 +25,66 @@ class Hash {
         $this->allArrays = array($this->mdArray, $this->ripemdArray, $this->shaArray, $this->tigerArray);
     }
     
+    public function getHash($input, $algorithm) {
+        if (in_array_r($algorithm, $allArrays)) {
+            if (in_array($algorithm, $mdArray)) {
+                return $this->getMDHash($input, $algorithm);
+            }
+            if (in_array($algorithm, $ripemdArray)) {
+                return $this->getRIPEMDHash($input, $algorithm);
+            }
+            if (in_array($algorithm, $shaArray)) {
+                return $this->getSHAHAsh($input, $algorithm);
+            }
+        }
+    }
+    
     public function getMDArray() {
         return $this->mdArray;
+    }
+    
+    public function getMDHash($input, $algorithm) {
+        $hashFamily = new HashMD();
+        switch ($algorithm) {
+            case "MD5":
+                return $hashFamily->getMD5Hash($input);
+        }
     }
     
     public function getRIPEMDArray() {
         return $this->ripemdArray;
     }
     
+    public function getRIPEMDHash($input, $algorithm) {
+        $hashFamily = new HashRIPEMD();
+        switch ($algorithm) {
+            case "RIPEMD128":
+                return $hashFamily->getRIPEMD128Hash($input);
+            case "RIPEMD160":
+                return $hashFamily->getRIPEMD160Hash($input);
+            case "RIPEMD256":
+                return $hashFamily->getRIPEMD256Hash($input);
+            case "RIPEMD320":
+                return $hashFamily->getRIPEMD320Hash($input);
+        }
+    }
+    
     public function getSHAArray() {
         return $this->shaArray;
+    }
+    
+    public function getSHAHash($input, $algorithm) {
+        $hashFamily = new HashSHA();
+        switch ($algorithm) {
+            case "SHA1":
+                return $hashFamily->getSHA1Hash($input);
+            case "SHA256":
+                return $hashFamily->getSHA256Hash($input);
+            case "SHA384":
+                return $hashFamily->getSHA384Hash($input);
+            case "SHA512":
+                return $hashFamily->getSHA512Hash($input);
+        }
     }
     
     public function getTigerArray() {
