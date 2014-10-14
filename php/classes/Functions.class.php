7f8fd11f48ec4e86b3f9d6b61a6d1c76692eea2d
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-14 21:22:11 +0200

    Removed unused functions

--- php/classes/Functions.class.php
+++ php/classes/Functions.class.php
@@ -9,16 +9,6 @@
  */
 class Functions {
     
-    /* Credits to elusive (http://stackoverflow.com/users/427328/elusive) */
-    public function in_array_r($needle, $haystack, $strict = false) {
-        foreach ($haystack as $item) {
-            if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
     /* Credits to Tim (http://stackoverflow.com/users/698511/tim) */
     public function array_key_exists_r($needle, $haystack) {
         if (array_key_exists($needle, $haystack)) {
