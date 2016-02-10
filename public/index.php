316d64c82ede022e5b74d1dc77c189bd9046fa90
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-02-10 15:21:56 +0100

    Changed folder name for source code

--- public/index.php
+++ public/index.php
@@ -13,17 +13,17 @@ require __DIR__ . '/../vendor/autoload.php';
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../HashGenerator/settings.php';
+$settings = require __DIR__ . '/../app/settings.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require __DIR__ . '/../HashGenerator/dependencies.php';
+require __DIR__ . '/../app/dependencies.php';
 
 // Register middleware
-require __DIR__ . '/../HashGenerator/middleware.php';
+require __DIR__ . '/../app/middleware.php';
 
 // Register routes
-require __DIR__ . '/../HashGenerator/routes.php';
+require __DIR__ . '/../app/routes.php';
 
 // Run!
 $app->run();
