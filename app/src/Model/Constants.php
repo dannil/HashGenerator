562ada026407b47459ea4cc2fadbc3ec41c5967a
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-10-26 23:02:21 +0200

    Prepare for version 1.2.1

--- app/src/Model/Constants.php
+++ app/src/Model/Constants.php
@@ -12,11 +12,11 @@ namespace HashGenerator\Model;
 class Constants {
     
     public function getVersion() {
-        return "1.2.0";
+        return "1.2.1";
     }
     
     public function getPublishDate() {
-    	return "2016-02-08";
+    	return "2016-10-26";
     }
     
 }
