7d2fe600bb5f7113f2d1f7ddc504da02acdfe295
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-11-09 13:32:22 +0100

    Updated test cases

--- tests/Model/ConstantsTest.php
+++ tests/Model/ConstantsTest.php
@@ -5,19 +5,13 @@ namespace HashGenerator\Test;
 use HashGenerator\Model\Constants;
 
 class ConstantsTest extends \PHPUnit_Framework_TestCase {
-
-	protected $constants;
-	
-	protected function setUp() {
-		$this->constants = new Constants();
-	}
 	
 	public function testGetVersion() {
-		$this->assertNotNull($this->constants->getVersion());
+		$this->assertNotNull(Constants::getVersion());
 	}
 
 	public function testGetPublishDate() {
-		$this->assertNotNull($this->constants->getPublishDate());
+		$this->assertNotNull(Constants::getPublishDate());
 	}
 
 
